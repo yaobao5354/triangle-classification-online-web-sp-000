5d07adb2ee623588a4e832dab1ba19cef9f31c41
yaoyu bao <yaobao5354@gmail.com>
2019-09-08 04:42:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :length1, :length2, :length3
+  
+  def initialize (length1,length2,length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end 
+  
+  def kind 
+    if self.length1 == self.length2 && self.length1  == self.length3
+      :equilateral
+    end 
+  end 
 end
